ff603a0fb23ae93f0e77ae910178c424bbcec4a3
Johnstein <joao.neto.feup@gmail.com>
2015-10-10 11:54:08 +0100

    Reading Textures

---
+++ Armadillo/Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeGenerator : MonoBehaviour {
+
+    public Texture2D tex;
+    public Rect sourceRect;
+
+	// Use this for initialization
+	void Start () {
+        Color[] test = ArrayFromTexture(tex, sourceRect);
+
+        foreach (Color c in test)
+        {
+            Debug.Log("Color = " + c.r + "," + c.g + "," + c.b);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    Color[] ArrayFromTexture(Texture2D tex, Rect sourceRect)
+    {
+        int x = Mathf.FloorToInt(sourceRect.x);
+        int y = Mathf.FloorToInt(sourceRect.y);
+
+        int width = Mathf.FloorToInt(sourceRect.width);
+        int height = Mathf.FloorToInt(sourceRect.height);
+
+        return (tex.GetPixels(x, y, width, height));
+
+    }
+}
